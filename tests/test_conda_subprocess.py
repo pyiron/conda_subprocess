f0f427dd109a8cc249a23362c07737b6306d5791
jan-janssen <jan.janssen@outlook.com>
2024-01-20 18:17:30 +0100

    switch to prefix_path

--- tests/test_conda_subprocess.py
+++ tests/test_conda_subprocess.py
@@ -5,22 +5,22 @@ from conda_subprocess import call, check_call, check_output, run, Popen
 
 class TestCondaSubprocess(TestCase):
     def test_call(self):
-        self.assertEqual(call("python --version", prefix_name="py312"), 0)
+        self.assertEqual(call("python --version", prefix_path="../py312"), 0)
 
     def test_check_call(self):
-        self.assertEqual(check_call("python --version", prefix_name="py312"), 0)
+        self.assertEqual(check_call("python --version", prefix_path="../py312"), 0)
 
     def test_check_output(self):
-        self.assertEqual(check_output("python --version", prefix_name="py312"), b'Python 3.12.1\n')
+        self.assertEqual(check_output("python --version", prefix_path="../py312"), b'Python 3.12.1\n')
 
     def test_check_output_universal_newlines(self):
-        self.assertEqual(check_output("python --version", prefix_name="py312", universal_newlines=True), 'Python 3.12.1\n')
+        self.assertEqual(check_output("python --version", prefix_path="../py312", universal_newlines=True), 'Python 3.12.1\n')
 
     def test_run(self):
-        self.assertEqual(run("python --version", prefix_name="py312").returncode, 0)
+        self.assertEqual(run("python --version", prefix_path="../py312").returncode, 0)
 
     def test_popen(self):
-        process = Popen("python --version", prefix_name="py312", stdout=PIPE)
+        process = Popen("python --version", prefix_path="../py312", stdout=PIPE)
         output = process.communicate()
         self.assertEqual(output[0], b'Python 3.12.1\n')
         self.assertIsNone(output[1])
