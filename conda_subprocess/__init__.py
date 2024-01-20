1e5cf7dad8e61ffb7a653334e84d0fd2ae43f70d
jan-janssen <jan.janssen@outlook.com>
2024-01-20 18:10:44 +0100

    add version

--- conda_subprocess/__init__.py
+++ conda_subprocess/__init__.py
@@ -1,2 +1,5 @@
 from conda_subprocess.process import Popen
 from conda_subprocess.interface import call, check_call, check_output, run
+from . import _version
+
+__version__ = _version.get_versions()["version"]
