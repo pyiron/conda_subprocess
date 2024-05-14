624b62def45beca95fd69bd1c6a27b594f71de3e
Jan Janssen <janssen@mpie.de>
2024-05-14 18:30:39 -0500

    add ruff linter

--- conda_subprocess/__init__.py
+++ conda_subprocess/__init__.py
@@ -3,3 +3,10 @@ from conda_subprocess.interface import call, check_call, check_output, run
 from . import _version
 
 __version__ = _version.get_versions()["version"]
+__all__ = [
+    Popen,
+    call,
+    check_call,
+    check_output,
+    run,
+]
