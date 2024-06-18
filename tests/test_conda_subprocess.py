a9a462185a4529a54f14e8122b115c481ff11bc9
Jan Janßen <janssen@mpie.de>
2024-06-18 19:29:48 +0200

    fix test

--- tests/test_conda_subprocess.py
+++ tests/test_conda_subprocess.py
@@ -51,9 +51,8 @@ class TestCondaSubprocess(TestCase):
         self.assertIsNone(output[1])
 
     def test_environment_variable(self):
-        print(
-            check_output(
+        self.assertTrue(
+            'TESTVAR=test' in check_output(
                 "env", prefix_path=self.env_path, env={"TESTVAR": "test"}, universal_newlines=True
             ).split("\n"),
         )
-        self.assertTrue(True)
