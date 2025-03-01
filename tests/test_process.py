37b42b34adad48b1f7d2ec95157bb3887ab51d26
Jan Janßen <janssen@mpie.de>
2025-03-01 13:19:19 +0100

    reduce

--- tests/test_process.py
+++ tests/test_process.py
@@ -9,7 +9,6 @@ from conda_subprocess.process import _locate_prefix_by_name, _check_prefix, _val
 class TestProcess(TestCase):
     def test_locate_prefix_by_name(self):
         self.assertEqual(context.root_prefix, _locate_prefix_by_name(name="root"))
-        self.assertEqual("py312", _locate_prefix_by_name(name="py312"))
         with self.assertRaises(EnvironmentNameNotFound):
             _locate_prefix_by_name(name="error")
 
@@ -17,10 +16,10 @@ class TestProcess(TestCase):
         self.assertEqual(context.root_prefix, _validate_prefix_name(prefix_name="root", ctx=context, allow_base=True))
         with self.assertRaises(CondaValueError):
             _validate_prefix_name(prefix_name="root", ctx=context, allow_base=False)
-        with self.assertRaises(EnvironmentNameNotFound):
-            _validate_prefix_name(prefix_name="error", ctx=context, allow_base=True)
         with self.assertRaises(CondaValueError):
             _validate_prefix_name(prefix_name="-", ctx=context, allow_base=True)
+        with self.assertRaises(EnvironmentNameNotFound):
+            _validate_prefix_name(prefix_name="error", ctx=context, allow_base=True)
 
     def test_check_prefix_name(self):
         self.assertEqual(context.default_prefix, _check_prefix())
